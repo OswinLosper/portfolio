d339cfdaa8112f3164c36ea2d7e1b927823a10c5
Oswin <oswin@iplatform.co.za>
2018-07-18 13:46:46 +0200

    feat: work and cv page

--- src/components/pages/cv/index.tsx
+++ src/components/pages/cv/index.tsx
@@ -67,47 +67,15 @@ export default class CVPage extends Component<ICVPageProps, any> {
            and more recently with desktop publishing software
           like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type
-            specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software
-            like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </Text>
 
     <MiddleSection>
           <WorkEperienceSection>
-                  <CVHeadings>
-                    WORK EXPERIENCE
-              </CVHeadings>
-              <Text>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-           when an unknown printer took a galley of type and scrambled it to make a type
-           specimen book. It has survived not only five centuries, but also the leap into
-           electronic typesetting, remaining essentially unchanged. It was popularised in
-           the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-           and more recently with desktop publishing software
-            like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </Text>
+                  ergergergerhger
           </WorkEperienceSection>
 
         <RightSideBarSection>
-          <CVHeadings>
-            SKILLS
-          </CVHeadings>
-          <Text>
-          Computer
-          Computer
-          Computer
-          Computer
-          Computer
-          Computer
-          Computer
-          Computer
-          </Text>
+  ergergergergerhg
         </RightSideBarSection>
 
         </MiddleSection>
