a29ab908add116bfe092324b2a1ede051061e699
Oswin <oswin@iplatform.co.za>
2018-08-07 10:40:42 +0200

    feat: remove design page

--- src/components/pages/designs/images.ts
+++
@@ -1,3 +0,0 @@
-export { default as BonlineFlyer } from '~/resources/projects/designs/bonline.jpg';
-export { default as Mines } from '~/resources/projects/designs/mines-flyers2.jpg';
-export { default as StrikaPoster } from '~/resources/projects/designs/strika-poster.jpg';
